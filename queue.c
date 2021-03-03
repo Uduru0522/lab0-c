6cbcfc4e799e98ba9c6dabb679f7136fadc31900
Uduru <kurasiki.homura@gmail.com>
2021-03-03 14:35:19 +0800

    Implement q_insert_head()

--- queue.c
+++ queue.c
@@ -128,9 +128,20 @@ bool q_insert_tail(queue_t *q, char *s)
  */
 bool q_remove_head(queue_t *q, char *sp, size_t bufsize)
 {
-    /* TODO: You need to fix up this code. */
-    /* TODO: Remove the above comment when you are about to implement. */
+    if (!q || !q->head) {
+        return false;
+    }
+
+    if (sp != NULL) {
+        strncpy(sp, q->head->value, bufsize - 1);
+        sp[bufsize - 1] = '\0';
+    }
+
+    list_ele_t *cursor = q->head;
     q->head = q->head->next;
+    free(cursor->value);
+    free(cursor);
+    --q->size;
     return true;
 }
 
